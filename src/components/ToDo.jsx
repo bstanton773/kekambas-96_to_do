92b30d54336477316a146704cdcf3d708452c99c
Brian <bstanton773@gmail.com>
2022-08-30 12:03:06 -0500

    add ToDoCard for each task

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ToDoCard from './ToDoCard';
 import ToDoForm from './ToDoForm';
 
 export default function ToDo() {
@@ -16,6 +17,9 @@ export default function ToDo() {
         <>
             <h1 className='text-center'>Brian's Awesome To-Do Generator</h1>
             <ToDoForm handleSubmit={updateTasks}/>
+            <div className='row mt-5'>
+                {tasks.map((task, idx) => <ToDoCard task={task} key={idx} />)}
+            </div>
         </>
     )
 }
